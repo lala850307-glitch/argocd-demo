601806f720c1004e0de7494b1584a0678b417cfb
layla <lala850307@example.com>
2026-04-14 14:12:42 +0800

    第二次變更

--- app/main.py
+++ app/main.py
@@ -7,5 +7,5 @@ app = FastAPI()
 def home():
     return {
         "version": os.getenv("APP_VERSION", "v4.0"),
-        "status": "謝謝老師，這是我的第一個 ArgoCD Demo！",
+        "status": "這是我的第一個 ArgoCD Demo！",
     }
